b87ba8de98433d97a83db5a32b52bbab23a5f5de
neerajac19 <neerajac22@gmail.com>
2024-08-28 17:52:45 +0530

    Login and Refresh Token

--- snipboxapp/admin.py
+++ snipboxapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Snipbox, Tag
+
+@admin.register(Snipbox)
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at', 'user')
+    search_fields = ('title', 'note')
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ('title',)
+
--- snipboxapp/models.py
+++ snipboxapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Snipbox(models.Model):
+    title = models.CharField(max_length=100)
+    note = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name='snipboxapp', blank=True)
+
+    def __str__(self):
+        return self.title
+
